fd4e6f52bd0be17878b6869eb2895dbb061dc054
limengning <limengning@pingpukj.com>
2021-08-06 17:21:15 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <wavesurfer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Wavesurfer from './components/Wavesurfer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Wavesurfer
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Wavesurfer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div ref="waveform"></div>
+</template>
+
+<script>
+const WaveSurfer = window.WaveSurfer
+export default {
+  data() {
+    return {
+      wavesurfer: null
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.wavesurfer = WaveSurfer.create({
+        container: this.$refs.waveform,
+        waveColor: 'violet',
+        progressColor: 'purple',
+        plugins: [
+          WaveSurfer.regions.create({
+            dragSelection: { slop: 100 }
+          })
+        ]
+      })
+      this.wavesurfer.load(
+        'http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3'
+      )
+    })
+  }
+}
+</script>
+
+<style>
+</style>
