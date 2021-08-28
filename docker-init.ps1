0b93950cf46cb354c211f610ededb5f5296ac92a
limengning <micheal_li@foxmail.com>
2021-08-29 07:38:32 +0800

    更新docker脚本

--- docker-init.ps1
+++ docker-init.ps1
@@ -5,12 +5,11 @@ mkdir -Force -p ~/docker/pgsql
 docker run -p 9000:9000 -p 9001:9001 --name local-minio `
   -e "MINIO_ACCESS_KEY=minio" `
   -e "MINIO_SECRET_KEY=wLiIgRc%qiC7~9qca6^" `
-  -v /tmp/data:/data `
-  -v /tmp/config:/root/.minio `
+  -v $pwd/tmp/data:/data `
+  -v $pwd/tmp/config:/root/.minio `
   -d minio/minio server /data --console-address ":9001"
 
 docker run -p 5432:5432 --name local-postgres `
  -e "POSTGRES_PASSWORD=FOzVhNc)LIz6b3^Pw~" `
- -e "PGDATA=/var/lib/postgresql/data/pgdata" `
- -v /tmp/pgdata:/var/lib/postgresql/data/pgdata `
- -d postgres
+ -v $pwd/tmp/pgdata:/var/lib/postgresql/data `
+ -d postgres:13
