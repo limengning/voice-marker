d32ca267c6775aaa91d544c8b44bf583d10811ee
limengning <li.mengning@outlook.com>
2021-09-04 08:24:02 +0800

    更新docker设置

--- docker-init.ps1
+++ docker-init.ps1
@@ -1,15 +1,8 @@
-mkdir -Force -p ~/docker/data
-mkdir -Force -p ~/docker/config
-mkdir -Force -p ~/docker/pgsql
-
-docker run -p 9000:9000 -p 9001:9001 --name local-minio `
+docker run -p 9000:9000 -p 9001:9001 --name minio `
   -e "MINIO_ACCESS_KEY=minio" `
-  -e "MINIO_SECRET_KEY=wLiIgRc%qiC7~9qca6^" `
-  -v $pwd/tmp/data:/data `
-  -v $pwd/tmp/config:/root/.minio `
+  -e "MINIO_SECRET_KEY=MINIO@local" `
   -d minio/minio server /data --console-address ":9001"
 
-docker run -p 5432:5432 --name local-postgres `
- -e "POSTGRES_PASSWORD=FOzVhNc)LIz6b3^Pw~" `
- -v $pwd/tmp/pgdata:/var/lib/postgresql/data `
+docker run -p 5432:5432 --name postgres `
+ -e "POSTGRES_PASSWORD=POSTGRES@local" `
  -d postgres:13
--- docker-init.sh
+++ docker-init.sh
@@ -1,16 +1,8 @@
-mkdir -p ~/docker/data
-mkdir -p ~/docker/config
-mkdir -p ~/docker/pgsql
-
 docker run -p 9000:9000 -p 9001:9001 --name local-minio \
   -e "MINIO_ACCESS_KEY=minio" \
-  -e "MINIO_SECRET_KEY=wLiIgRc%qiC7~9qca6^" \
-  -v ~/docker/data:/data \
-  -v ~/docker/config:/root/.minio \
+  -e "MINIO_SECRET_KEY=MINIO@local" \
   -d minio/minio server /data --console-address ":9001"
 
 docker run -p 5432:5432 --name local-postgres \
- -e "POSTGRES_PASSWORD=FOzVhNc)LIz6b3^Pw~" \
- -e "PGDATA=/var/lib/postgresql/data/pgdata" \
- -v ~/docker/pgsql:/var/lib/postgresql/data \
+ -e "POSTGRES_PASSWORD=POSTGRES@local" \
  -d postgres
