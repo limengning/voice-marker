ab22954351bbf41cd61d8888ab9fd89513cb6dba
limengning <li.mengning@outlook.com>
2021-09-06 18:07:00 +0800

    增加导出

--- api/src/main/java/com/limengning/marker/service/ProjectService.java
+++ api/src/main/java/com/limengning/marker/service/ProjectService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.limengning.marker.entity.FileEntity;
 import com.limengning.marker.entity.MarkEntity;
@@ -19,7 +21,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -49,27 +54,27 @@ public class ProjectService extends ServiceImpl<ProjectMapper, ProjectEntity> {
 
     public void export(Integer projectId, HttpServletResponse response) throws IOException {
         ProjectEntity project = getById(projectId);
+        response.addHeader("Content-Type", "application/octet-stream");
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(project.getName(), StandardCharsets.UTF_8) + ".csv");
         List<MarkFieldEntity> markFields = markFieldService.getFields(projectId);
         long pageIndex = 0L;
         long pageSize = 100L;
         PrintWriter writer = response.getWriter();
         StringBuilder builder = new StringBuilder("项目名称,文件名称,开始,结束");
-        for (MarkFieldEntity field :
-                markFields) {
+        for (MarkFieldEntity field : markFields) {
             builder.append(",");
             builder.append(field.getFieldDisplayText());
         }
+        builder.append("\n");
         writer.write(builder.toString());
         while (true) {
             IPage<FileEntity> page = fileService.page(pageIndex, pageSize, projectId);
-            if (page.getSize() < pageSize) {
+            if (page.getRecords().size() == 0) {
                 break;
             }
-            for (FileEntity file :
-                    page.getRecords()) {
+            for (FileEntity file : page.getRecords()) {
                 List<MarkEntity> marks = markService.list(file.getId());
-                for (MarkEntity mark :
-                        marks) {
+                for (MarkEntity mark : marks) {
                     builder = new StringBuilder(project.getName());
                     builder.append(",");
                     builder.append(file.getName());
@@ -78,9 +83,23 @@ public class ProjectService extends ServiceImpl<ProjectMapper, ProjectEntity> {
                     builder.append(",");
                     builder.append(mark.getEnd());
                     String comment = mark.getComment();
+                    if (StringUtils.isBlank(comment)) {
+                        comment = "{}";
+                    }
+                    ObjectMapper mapper = new ObjectMapper();
+                    final JsonNode jsonNode = mapper.readTree(comment);
+                    for (MarkFieldEntity field : markFields) {
+                        builder.append(",");
+                        String fieldName = field.getFieldName();
+                        if (jsonNode.has(fieldName)) {
+                            builder.append(jsonNode.get(fieldName).asText());
+                        }
+                    }
+                    builder.append("\n");
+                    writer.write(builder.toString());
+                    response.flushBuffer();
                 }
             }
-            response.flushBuffer();
             pageIndex += pageSize;
         }
     }
